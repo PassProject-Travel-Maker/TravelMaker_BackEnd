b153aebefdc3c5850ae2487c353c8dd45b0bdd55
minkyung7365 <minkyung7365@gmail.com>
2024-05-17 08:55:28 +0900

    [DEL]auth controller

--- src/main/java/com/ssafy/backend/domain/auth/controller/AuthController.java
+++ src/main/java/com/ssafy/backend/domain/auth/controller/AuthController.java
@@ -2,7 +2,6 @@ package com.ssafy.backend.domain.auth.controller;
 
 import com.ssafy.backend.domain.auth.dto.MemberDto.*;
 import com.ssafy.backend.domain.auth.model.Member;
-import com.ssafy.backend.domain.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,23 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/auth")
-@RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthService authService;
-
-    @PostMapping("/join")
-    public ResponseEntity<?> join(@RequestBody JoinRequestDto joinRequestDto) {
-        String token = authService.join(joinRequestDto);
-
-        if(token == null)
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body("회원가입 실패");
-
-        return ResponseEntity.ok(token);
-    }
 
 }
--- src/main/java/com/ssafy/backend/domain/auth/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.backend.domain.auth.service;
-
-import com.ssafy.backend.domain.auth.dto.MemberDto.*;
-
-public interface AuthService {
-
-    String join(JoinRequestDto joinRequestDto);
-    String login(FindMemberByIdDto loginInfo);
-}

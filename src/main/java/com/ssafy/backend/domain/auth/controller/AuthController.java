b46dfea9eac5c5f6e2b436a2527d606b8691c443
minkyung <minkyung7365@gmail.com>
2024-05-20 02:12:01 +0900

    [ADD]회원 정보 수정 modify controller (진행중)

--- src/main/java/com/ssafy/backend/domain/auth/controller/AuthController.java
+++ src/main/java/com/ssafy/backend/domain/auth/controller/AuthController.java
@@ -29,7 +29,6 @@ public class AuthController {
 
 	// 로그인
 	@PostMapping("/login")
-//		@ResponseBody
 	public ResponseEntity<?> login(@RequestBody LoginRequestDto loginInfo) {
 		System.out.println(loginInfo.getId());
 		String token = authService.login(loginInfo);
@@ -38,12 +37,23 @@ public class AuthController {
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("아이디 또는 비밀번호가 잘못되었습니다.");
 
 		return ResponseEntity.ok(token);
+	}
 
-//			TokenDto tokenDto = new TokenDto(token);
-//			return ResponseEntity.ok(tokenDto);
+	// 회원 정보 수정
+	@PatchMapping("/modify")
+	public ResponseEntity<?> modify(
+			@RequestHeader("Authorization") String tokenHeader,
+			@RequestBody String password) {
+
+		String memberId = jwtUtil.getIdFromToken(tokenHeader.substring(7));
+		if (memberId == null)
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않은 사용자입니다.");
+
+		return null;
 	}
 
 	// 회원 탈퇴
+	// TODO: RequestBody DTO로 변경
 	@DeleteMapping("/delete")
 	public ResponseEntity<?> delete(
 			@RequestHeader("Authorization") String tokenHeader,

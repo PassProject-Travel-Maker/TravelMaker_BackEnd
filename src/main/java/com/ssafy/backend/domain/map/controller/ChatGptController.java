f1c491b3ee082868dedffd33808436d31d678067
minkyung <minkyung7365@gmail.com>
2024-05-21 20:08:18 +0900

    [MOD]chatGPT recommend controller http method 수정

--- src/main/java/com/ssafy/backend/domain/map/controller/ChatGptController.java
+++ src/main/java/com/ssafy/backend/domain/map/controller/ChatGptController.java
@@ -4,10 +4,7 @@ import com.ssafy.backend.domain.map.dto.ChatGptDto.*;
 import com.ssafy.backend.domain.map.service.ChatGptService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,7 +21,8 @@ public class ChatGptController {
             **[ => <button class="">
             ]** => </button>
     */
-    @GetMapping("/recommend")
+//    @GetMapping("/recommend")
+    @PostMapping("/recommend")
     public ResponseEntity<?> recommend(@RequestBody ChatGptRequestDto chatGptRequestDto) {
         String result = chatGptService.makePrompt(chatGptRequestDto);
         return ResponseEntity.ok(result);
